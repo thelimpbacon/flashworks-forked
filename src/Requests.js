8ff0d79b744f65ce5471576c4cc7769a0ba4fd89
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2022-05-05 15:18:28 +0800

    add sorter

--- src/Requests.js
+++ src/Requests.js
@@ -1,13 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { getRequests } from "./Api";
 
-function compareHeight(a, b) {
+const compareHeight = (a, b) => {
   return a.height - b.height;
-}
+};
+
+const slicer = (array, start, end) => array.slice(start, end);
+
+const heightSorter = (array) => {
+  if (!array) return;
+  array.sort(function (a, b) {
+    return a - b;
+  });
+  let right = array.slice(array.length / 2, array.length).reverse();
+  let left = array.slice(0, array.length / 2);
+  array = left.concat(right);
+  return array;
+};
 
 const Requests = () => {
   const [rowReference, setRowReference] = useState(undefined);
   const [players, setPlayers] = useState(undefined);
+  const [rows, setRows] = useState({
+    row1: undefined,
+    row2: undefined,
+    row3: undefined,
+    row4: undefined,
+    row5: undefined,
+  });
 
   useEffect(() => {
     getRequests().then((data) => {
@@ -22,9 +42,40 @@ const Requests = () => {
     });
   }, []);
 
-  console.log(players);
+  useEffect(() => {
+    if (!rowReference) return;
+    setRows({
+      row1: slicer(players, 0, rowReference.row_1),
+      row2: slicer(
+        players,
+        rowReference.row_1,
+        rowReference.row_1 + rowReference.row_2
+      ),
+      row3: slicer(
+        players,
+        rowReference.row_1 + rowReference.row_2,
+        rowReference.row_2 + rowReference.row_3
+      ),
+      row4: slicer(
+        players,
+        rowReference.row_2 + rowReference.row_3,
+        rowReference.row_3 + rowReference.row_4
+      ),
+      row5: slicer(
+        players,
+        rowReference.row_3 + rowReference.row_4,
+        rowReference.row_4 + rowReference.row_5
+      ),
+    });
+  }, [players]);
 
-  return <div>Your code will go here</div>;
+  return (
+    <div>
+      <div>{JSON.stringify(heightSorter(rows.row1))}</div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Requests;
